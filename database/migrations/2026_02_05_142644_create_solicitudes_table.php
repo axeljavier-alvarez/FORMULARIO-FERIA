cef61bec33c2b70492d99d97d10dcb25340f115a
axeljavier-alvarez <axjavier4alvarez44@gmail.com>
2026-02-05 08:52:04 -0600

    migraciones base de datos

---
+++ database/migrations/2026_02_05_142644_create_solicitudes_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('solicitudes', function (Blueprint $table) {
+            $table->id();
+            $table->string('sobre_mi', 1000);
+            $table->string('nombres', 100);
+            $table->string('apellidos', 100);
+            $table->string('email', 50);
+            $table->string('telefono', 20);
+            $table->string('dpi', 15);
+            $table->char('sexo', 1);
+            $table->date('fechanac');
+            $table->foreignId('departamento_id', 3)->constrained('departamentos')->restrictOnDelete(); 
+            $table->foreignId('municipio_id', 3)->constrained('municipios')->restrictOnDelete();
+            $table->string('zona', 3)->nullable();
+            $table->foreignId('estado_id', 3)->constrained('estados')->restrictOnDelete();
+            $table->string('hash', 50);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('solicitudes');
+    }
+};
---
+++ database/migrations/2026_02_05_144526_create_documentos_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('documentos', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('solicitud_id', 3)->constrained('solicitudes')->restrictOnDelete(); 
+            $table->string('ruta', 100);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('documentos');
+    }
+};
